ff88fa039b502f702551458c731a0d2fbb51654c
kurvidah <hikaru.kp@gmail.com>
2025-10-12 20:10:36 +0700

    feat: Merge cleanups

--- app/providers/AppProviders.tsx
+++ app/providers/AppProviders.tsx
@@ -3,18 +3,19 @@
 import { ThemeProvider } from 'next-themes';
 import { ReactQueryProvider } from './ReactQueryProvider';
 import { AuthProvider } from './AuthProvider';
+import { Session } from '@supabase/supabase-js';
 
 export function AppProviders({
   children,
   initialSession,
 }: {
   children: React.ReactNode;
-  initialSession?: any;
+  initialSession?: Session | null;
 }) {
   return (
     <ReactQueryProvider>
       <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-        <AuthProvider initialSession={initialSession}>
+        <AuthProvider initialSession={initialSession || null}>
           {children}
         </AuthProvider>
       </ThemeProvider>
